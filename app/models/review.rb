d52a1bd32d5f789abbf7a43c76820d7d3950298c
rm <biffbangpow767@yahoo.com>
2017-11-17 10:02:32 -0800

    have the capability to create, read,update, and destroy products and have the capability to read reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
   validates :author, :presence => true
-  validates :content_body, :presence => true
+  validates :content_body, :presence => true, :length => { :minimum => 50, :maximum => 250 }
   validates :rating, :presence => true
   validates :product_id, :presence => true
 
